83a54e960277b6839075a93e374af3c3e8b37a12
Denis Perchenko <plachenko@gmail.com>
2021-09-15 12:12:55 -0400

    back to basics

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,70 @@
 <script lang="ts">
-import { onMount } from "svelte";
+	import { onMount } from "svelte";
+import { claim_text, xlink_attr } from "svelte/internal";
+	
 	let canvas;
+	let ctx;
+	let capture;
+	let menu;
+
+	let stroke;
+	let lp;
 
 	onMount(()=>{
-		console.log(canvas);
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		ctx = canvas.getContext('2d');
+		
+		//center logo
+		menu.style.left = (window.innerWidth / 2) - (menu.getBoundingClientRect().left/2) + 'px';
+
+		capture.addEventListener('pointermove', pmv);
+		capture.addEventListener('pointerdown', pdn);
+		capture.addEventListener('pointerup', pup);
+		capture.addEventListener('contextmenu', (e)=>{
+			e.preventDefault();
+		});
 	});
+
+	function draw(x = 0, y = 0){
+		ctx.fillRect(x, y, 10, 10);
+
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.lineTo(lp.x, lp.y);
+		ctx.lineTo(lp.x+10, lp.y+10);
+		ctx.lineTo(x+10, y+10);
+		// ctx.lineTo(lp.x+10, lp.y);
+		ctx.closePath();
+		ctx.fill();
+
+		lp = {x: x, y: y};
+	}
+
+	function pdn(e){
+		stroke = {
+			x: e.clientX,
+			y: e.clientY
+		}
+		lp = stroke;
+	}
+
+	function pmv(e){
+		if(stroke){
+			draw(e.clientX, e.clientY);
+			// console.log(e);
+		}
+	}
+
+	function pup(e){
+		stroke = null;
+	}
 </script>
 
 <main>
-	<div id="capture"></div>
+	<div bind:this={capture} id="capture"></div>
 	<canvas bind:this={canvas} />
+	<div bind:this={menu} id="flipslide-logo">flipslide</div>
 </main>
 
 <style>
@@ -18,8 +73,22 @@ import { onMount } from "svelte";
 		width: 100%;
 		height: 100%;
 		}
-		#capture{
+		#flipslide-logo{
+			border: 1px solid;
+			padding: 5px 10px;
+			position: absolute;
+			float: left;
+			z-index: 9999;
+			font-weight: bold;
+			bottom: 5px;
+			user-select: none;
+			cursor: pointer;
+		}
+		#canvas{
 			background-color:#F00;
+		}
+		#capture{
+			z-index: 9999;
 			width: 100%;
 			height: 100%;
 			position: absolute;
