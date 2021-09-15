b959f10803d5e4c22488f11535c78434ce53fbfc
Denis Perchenko <plachenko@gmail.com>
2021-09-14 21:48:26 -0400

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,27 @@
 <script lang="ts">
-	export let name: string;
+import { onMount } from "svelte";
+	let canvas;
+
+	onMount(()=>{
+		console.log(canvas);
+	});
 </script>
 
 <main>
-	<canvas />
+	<div id="capture"></div>
+	<canvas bind:this={canvas} />
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+		position: relative;
+		width: 100%;
+		height: 100%;
 		}
-	}
+		#capture{
+			background-color:#F00;
+			width: 100%;
+			height: 100%;
+			position: absolute;
+			}
 </style>
